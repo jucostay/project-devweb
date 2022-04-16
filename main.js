a232b571a553f5df4527e18e78af96bd33b6678c
Julia Costa <ramosjulia716@gmail.com>
2022-04-16 15:27:06 -0300

    Ajustando responsividade, removendo comentarios e ajustando detalhes

--- main.js
+++ main.js
@@ -47,3 +47,54 @@ const scrollReveal = ScrollReveal({
     duration: 700,
     reset: true
 })
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `, { interval: 100 }
+)
+
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active')
+        } else {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active')
+        }
+    }
+}
+
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+    activateMenuAtCurrentSection()
+})
